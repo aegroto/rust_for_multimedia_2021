d1c50033bf110bde636a25db6d586f28c91076dd
aegroto <aegroto@protonmail.com>
2022-01-10 10:42:30 +0100

    canny input image loading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use image::ImageError;
+
+fn main() -> Result<(), ImageError> {
+    // Load image and convert to Luma8
+    let image_reader = image::io::Reader::open("test_assets/myownlena.jpg")?;
+    let image = image_reader.decode()?;
+    let image_luma = image.into_luma8();
+    image_luma.save("test_outputs/myownlena_canny.jpg").unwrap();
+
+    Ok(())
 }
